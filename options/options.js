4a1e3e65ce9a6b2a6d4ec16264da48efb9ce0833
Puspdeep Singh <singhpushpdeep09@gmail.com>
2023-08-17 12:57:29 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "demo-42897",
-  url: "https://demo-42897.botics.co",
-  api: "https://demo-42897.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "demo-42897",
+  "url": "https://demo-42897.botics.co",
+  "api": "https://demo-42897.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "chatListing"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
